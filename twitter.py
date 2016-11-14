64626b6c2fd3a3abe0fc8a0d5e0f02e05129c5cd
angieMutava <mutavaangela@yahoo.com>
2016-11-14 15:25:14 +0300

    does the function of getting the tweets

--- twitter.py
+++ twitter.py
@@ -23,11 +23,11 @@ class StdOutListener(StreamListener):
 
 if __name__ == '__main__':
 
-    #This handles Twitter authetification and the connection to Twitter Streaming API
+    #This handles Twitter authentication and the connection to Twitter Streaming API
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
 
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'andela', 'bootcamp'
     stream.filter(track=['python', 'andela', 'bootcamp'])
