ac7d82c4c497a71d6a63d400413f3da97e30c37c
angieMutava <mutavaangela@yahoo.com>
2016-11-16 09:49:25 -0800

    changed the constant parameters

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-access_token = "715507251373555716-D9M6UiJf8mnxA0My37Zx5FxpQ0NKftg"
-access_secret = "L7UFMQmtAw3cNI3kmOsRsaHIYGd2qKKhmV0PAgaprhMom "
-consumer_key = "E2tKHjdRbfA3RyRCNXd4YMOTD"
-consumer_secret = "mygvV8LsFZgwAgye6ZpqNpQ5fHBOo1vOd9Q6iaLDczROC760jd "
+access_token = " 715507251373555716-yte4B7br0bX32gzW9uEXYO5iFBydLT3"
+access_secret = " f9qEf9uH7EWDu81RcTewQf0uiU9rvU81M20I4Te2QG8M3 "
+consumer_key = " B56wDYu1LTAox2b1jqiNoFMhZ"
+consumer_secret = " UtFbMlVxeB1pXO14jWpu9PNVCfX8g50cTmkJE0bC8aeNzsN0kg "
