cc05ce7d75cd688d129076e15c7f193963fcd3dd
angieMutava <mutavaangela@yahoo.com>
2016-11-16 08:53:05 -0800

    added config

---
+++ config.py
@@ -0,0 +1,4 @@
+access_token = "715507251373555716-D9M6UiJf8mnxA0My37Zx5FxpQ0NKftg"
+access_secret = "L7UFMQmtAw3cNI3kmOsRsaHIYGd2qKKhmV0PAgaprhMom "
+consumer_key = "E2tKHjdRbfA3RyRCNXd4YMOTD"
+consumer_secret = "mygvV8LsFZgwAgye6ZpqNpQ5fHBOo1vOd9Q6iaLDczROC760jd "
